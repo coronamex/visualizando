c529da983956d1ef8a5c06a66ab9577de6fa49b0
surh <sur00mx@gmail.com>
2020-04-04 19:44:58 -0700

    Optimizando R_0

--- sir/sir.r
+++ sir/sir.r
@@ -24,35 +24,88 @@ sir <- function(time, state, parameters) {
   })
 }
 
-N_pob <- 135552447
-casos_confirmados <- 1
-n_dias <- 200
+args <- list(tabla_sintomas = "../datos/ssa_dge/tabla_casos_confirmados.csv",
+             dias_retraso = 15)
 
-# Condiciones iniciales
-t_0 <- c(S = (N_pob - casos_confirmados)/N_pob,
-          E = 0,
-          I = casos_confirmados / N_pob,
-          R = 0.0)
-# Parámetros
-parametros <- c(R_t = 2.2, T_inf = 3, T_inc = 5)
-## Time frame 
-dias <- seq(0, n_dias, by = 1)
+# Leer 
+Tab <- read_csv(args$tabla_sintomas,
+                col_types = cols(estado = col_character(),
+                                 sexo = col_character(),
+                                 edad = col_number(),
+                                 fecha_sintomas = col_date(format = "%d/%m/%Y"),
+                                 procedencia = col_character(),
+                                 fecha_llegada = col_date(format = "%d/%m/%Y")))
+Tab <- table(Tab$fecha_sintomas)
+Tab <- tibble(fecha = names(Tab) %>% as.Date("%Y-%m-%d"),
+              casos_nuevos = as.vector(Tab)) %>%
+  mutate(casos_acumulados = cumsum(casos_nuevos)) %>%
+  filter(fecha <= (max(fecha) - args$dias_retraso)) %>%
+  mutate(dia = as.numeric(fecha - min(fecha)))
+Tab
+max(Tab$fecha) - min(Tab$fecha)
 
-# Usar ode para resolver, convertir a tibble
-pred <- ode(y = t_0, times = dias, func = sir, parms = parametros)
-pred <- tibble(dia = pred[,"time"],
-       S = pred[,"S"],
-       E = pred[,'E'],
-       I = pred[,"I"],
-       R = pred[,"R"])
+sir_simular <- function(t_0, parametros, n_dias, FUN = sir){
+  # funcion
+  FUN <- match.fun(FUN)
+  
+  # Definir dias
+  dias <- seq(0, n_dias, by = 1)
+  
+  # Usar ode para resolver, convertir a tibble
+  pred <- ode(y = t_0, times = dias, func = FUN, parms = parametros)
+  pred <- tibble(dia = pred[,"time"],
+                 S = pred[,"S"],
+                 E = pred[,'E'],
+                 I = pred[,"I"],
+                 R = pred[,"R"])
+  
+  return(pred)
+}
+
+sir_optmizable <- function(R_0, real, pob, T_inf = 3, T_inc = 5){
+  # Definir Condiciones iniciales
+  casos_confirmados <- real$casos_acumulados[real$dia == 0]
+  t_0 <- c(S = (pob - casos_confirmados) / pob,
+           E = 0,
+           I = casos_confirmados / pob,
+           R = 0.0)
+  # Parámetros
+  parametros <- c(R_t = R_0, T_inf = T_inf, T_inc = T_inc)
+  
+  pred <- sir_simular(t_0 = t_0, parametros = parametros,
+                      n_dias = max(real$dia), FUN = sir)
+  
+  ss <- pred %>%
+    filter(dia <= 49) %>%
+    mutate(casos = I*pob) %>%
+    select(dia, casos) %>%
+    left_join(real %>% select(dia, casos_nuevos), by = "dia") %>%
+    mutate(casos_nuevos = replace(casos_nuevos, is.na(casos_nuevos), 0)) %>%
+    mutate(diff2 = (casos_nuevos - casos) ^ 2) %>%
+    select(diff2) %>%
+    unlist %>%
+    sum
+  
+  return(ss)
+}
+
+pob <- 135552447
+
+R_0_optim <- optim(2, fn = sir_optmizable,
+                   method = "Brent", lower = 0.5, upper = 5,
+                   real = Tab, pob = pob, T_inf = 3, T_inc = 5)
+R_0_optim
 
 
+Tab
 p1 <- pred %>%
   pivot_longer(-dia) %>%
   filter(name != "S") %>%
   filter(name != "R") %>%
   ggplot(aes(x = dia, y = N_pob * value, group = name)) +
   geom_line(aes(col = name)) +
+  xlim(c(0,50)) +
+  ylim(c(0,1000)) +
   theme(panel.background = element_blank())
 p1
 
