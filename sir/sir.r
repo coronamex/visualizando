ab7e0b7e35342328fb98a2ed4557827be779a85a
surh <sur00mx@gmail.com>
2020-04-10 09:23:03 -0700

    Anadiendo periodo de ajuste, y simular hasta fecha actual

--- sir/sir.r
+++ sir/sir.r
@@ -23,22 +23,11 @@ library(deSolve)
 sir <- function(time, state, parameters) {
   with(as.list(c(state, parameters)), {
     
-    # # SIR
-    # dS <- -beta * S * I
-    # dI <-  beta * S * I - gamma * I
-    # dR <-                 gamma * I
-    # return(list(c(dS, dI, dR)))
-    
     # SEIR
     dS <- -(R_t / T_inf) * (I * S)
     dE <- (R_t / T_inf) * (I * S) - ((1 / T_inc) * E)
     dI <- ((1 / T_inc) * E) - ((1 / T_inf) * I)
     dR <- (1 / T_inf) * I
-    
-    # dS <- max(0, dS)
-    # dE <- max(0, dE)
-    # dI <- max(0, dI)
-    # dR <- max(0, dR)
     
     return(list(c(dS, dE, dI, dR)))
   })
@@ -64,13 +53,18 @@ sir_simular <- function(t_0, parametros, n_dias, FUN = sir){
 }
 
 sir_optmizable <- function(R_0, real, pob, T_inf = 3, T_inc = 5){
+  # real <- Tab
+  # T_inf = 3
+  # T_inc = 5
+  # R_0 = 2
   
   # Definir Condiciones iniciales
-  casos_confirmados <- real$casos_acumulados[real$dia == 0]
-  t_0 <- c(S = (pob - casos_confirmados) / pob,
+  I_actuales <- real$I_actuales[real$dia == 0]
+  R_actuales <- real$R_actuales[real$dia == 0]
+  t_0 <- c(S = (pob - I_actuales - R_actuales) / pob,
            E = 0,
-           I = casos_confirmados / pob,
-           R = 0.0)
+           I = I_actuales / pob,
+           R = R_actuales / pob)
   # Parámetros
   parametros <- c(R_t = R_0, T_inf = T_inf, T_inc = T_inc)
   
@@ -78,12 +72,10 @@ sir_optmizable <- function(R_0, real, pob, T_inf = 3, T_inc = 5){
                       n_dias = max(real$dia), FUN = sir)
   
   ss <- pred %>%
-    mutate(casos_acumulados = pob*(I + R)) %>%
-    mutate(casos = c(casos_confirmados, diff(casos_acumulados))) %>%
-    select(dia, casos) %>%
-    left_join(real %>% select(dia, casos_nuevos), by = "dia") %>%
-    mutate(casos_nuevos = replace(casos_nuevos, is.na(casos_nuevos), 0)) %>%
-    mutate(diff2 = (casos_nuevos - casos) ^ 2) %>%
+    mutate(casos_acumulados = floor(pob*(I + R))) %>%
+    select(dia, casos_acumulados) %>%
+    left_join(real %>% select(dia, casos_acumulados), by = "dia") %>%
+    mutate(diff2 = (casos_acumulados.x - casos_acumulados.y) ^ 2) %>%
     select(diff2) %>%
     unlist %>%
     sum
@@ -92,12 +84,25 @@ sir_optmizable <- function(R_0, real, pob, T_inf = 3, T_inc = 5){
 }
 
 encontrar_R_0 <- function(Tab, dias_retraso = 15,
+                          periodo_ajuste = 28,
                           T_inc =c(4.1, 5.2, 7.9),
                           T_inf = c(1.5, 2.9, 6),
-                          pob = 135552447){
+                          pob = 135552447,
+                          fecha_final = Sys.Date()){
+  
+  # dias_retraso = 15
+  # periodo_ajuste = 28
+  # T_inc =c(4.1, 5.2, 7.9)
+  # T_inf = c(1.5, 2.9, 6)
+  # pob = 135552447
+  # fecha_final = Sys.Date()
+  
   Tab <- Tab %>%
-    filter(fecha <= (max(fecha) - dias_retraso)) %>%
-    mutate(dia = as.numeric(fecha - min(fecha)))
+    mutate(R_actuales = lag(casos_acumulados, 2, default = 0)) %>%
+    mutate(I_actuales = casos_acumulados - R_actuales) %>%
+    filter(fecha <= (fecha_final - dias_retraso)) %>%
+    filter(fecha >= (fecha_final - dias_retraso - periodo_ajuste + 1)) %>%
+    mutate(dia = as.numeric(fecha - min(fecha))) 
   
   # Optimizar en matriz de parámetros
   Dat <- expand.grid(T_inc = T_inc, T_inf = T_inf) %>%
@@ -111,14 +116,28 @@ encontrar_R_0 <- function(Tab, dias_retraso = 15,
   Dat
 }
 
-simular_multiples_modelos <- function(modelos, FUN, real, pob){
+simular_multiples_modelos <- function(modelos, FUN, real, pob, n_dias = 43,
+                                      fecha_final = Sys.Date()){
+  # n_dias = args$dias_retraso + args$periodo_ajuste
+  # modelos = R_hat
+  # FUN = sir
+  # real = Tab
+  # fecha_final <- Sys.Date()
+  
   FUN <- match.fun(FUN)
   # Definir t_0
-  casos_confirmados <- real$casos_acumulados[real$dia == 0]
-  t_0 <- c(S = (pob - casos_confirmados) / pob,
+  real <- real %>%
+    mutate(R_actuales = lag(casos_acumulados, 2, default = 0)) %>%
+    mutate(I_actuales = casos_acumulados - R_actuales) %>%
+    filter(fecha >= (fecha_final - n_dias + 1)) %>%
+    mutate(dia = as.numeric(fecha - min(fecha)))
+  
+  I_actuales <- real$I_actuales[real$dia == 0]
+  R_actuales <- real$R_actuales[real$dia == 0]
+  t_0 <- c(S = (pob - I_actuales - R_actuales) / pob,
            E = 0,
-           I = casos_confirmados / pob,
-           R = 0.0)
+           I = I_actuales / pob,
+           R = R_actuales / pob)
   
   sims <- modelos %>%
     bind_cols(modelo = paste0("m", 1:nrow(modelos))) %>%
@@ -128,7 +147,7 @@ simular_multiples_modelos <- function(modelos, FUN, real, pob){
         mutate(casos_acumulados = floor(pob * (I + R))) %>%
         select(dia, casos_acumulados) %>%
         mutate(modelo = modelo)
-    }, n_dias = max(real$dia)+2, FUN = FUN, t_0 = t_0)
+    }, n_dias = n_dias , FUN = FUN, t_0 = t_0)
   sims
 }
 
@@ -137,7 +156,7 @@ args <- list(tabla_sintomas = "../datos/ssa_dge/tabla_casos_confirmados.csv",
              reportes_diarios = "../datos/ssa_dge/reportes_diarios.csv",
              dias_retraso = 15,
              dir_salida = "../sitio_hugo/static/imagenes/",
-             fecha_hoy = "2020-04-09")
+             periodo_ajuste = 7)
 
 # Leer 
 Tab <- read_csv(args$tabla_sintomas,
@@ -156,40 +175,48 @@ Tab <- tibble(fecha = names(Tab) %>% as.Date("%Y-%m-%d"),
 pob <- 135552447
 T_inc <- c(2, 3, 4, 5, 6, 7, 8)
 T_inf <- c(1, 2, 3, 4, 5, 6)
+pob <- 127000000
+T_inc <- c(5,5.2,5.4)
+T_inf <- c(2.5,3,3.5)
 
 R_hat <- encontrar_R_0(Tab, dias_retraso = args$dias_retraso,
+                       periodo_ajuste = args$periodo_ajuste,
                        T_inc = T_inc, T_inf = T_inf, pob = pob)
 R_hat
 
 # Simular con parámetros estimados
-sims <- simular_multiples_modelos(modelos = R_hat, FUN = sir, real = Tab, pob = pob)
+sims <- simular_multiples_modelos(modelos = R_hat, FUN = sir, real = Tab, pob = pob,
+                                  n_dias = args$dias_retraso + args$periodo_ajuste)
 ctds <- read_csv(args$reportes_diarios) %>%
   select(fecha, casos_acumulados) %>%
   mutate(modelo = "Confirmado")
 
 p1 <- Tab %>%
+  filter(fecha >= (Sys.Date() - args$dias_retraso - args$periodo_ajuste + 1)) %>%
+  mutate(dia = as.numeric(fecha - min(fecha))) %>%
   select(dia, casos_acumulados) %>%
   mutate(modelo = "real") %>%
   bind_rows(sims) %>%
-  mutate(fecha = min(Tab$fecha) + dia) %>%
+  mutate(fecha = (Sys.Date() - args$dias_retraso - args$periodo_ajuste + 1) + dia) %>%
   select(-dia) %>%
   bind_rows(ctds) %>%
   mutate(grupo = "Estimado (SEIR)") %>%
   mutate(grupo = replace(grupo, modelo == "real", "Inicio de síntomas")) %>%
   mutate(grupo = replace(grupo, modelo == "Confirmado", "Confirmado")) %>%
-  filter(fecha >= as.Date("2020-02-27")) %>%
+  filter(fecha >= (Sys.Date() - args$dias_retraso - args$periodo_ajuste + 1)) %>%
   
-  filter(modelo != "m2") %>%
+  # filter(modelo != "m2") %>%
   
   ggplot(aes(x = fecha, y = casos_acumulados, group = modelo)) +
   geom_line(aes(col = grupo, size = grupo)) +
+  geom_vline(xintercept = Sys.Date() - args$dias_retraso) +
   scale_color_brewer(palette = "Dark2", name = NULL) +
   scale_size_manual(values = c(2, 0.1, 2)) +
   guides(size = FALSE) +
   ylab("Casos acumulados") +
   xlab("Fecha") +
   AMOR::theme_blackbox() +
-  ylim(c(0,18000)) +
+  # ylim(c(0,18000)) +
   scale_y_log10() +
   theme(panel.background = element_blank(),
         panel.border = element_rect(fill = NA, color = "black", size = 3),
