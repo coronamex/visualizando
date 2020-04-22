44a127838c72a89a1e286b6d7f23c68e96b66daf
surh <sur00mx@gmail.com>
2020-04-21 18:44:12 -0700

    Limpiar

--- casos/centinela_preliminar.r
+++ casos/centinela_preliminar.r
@@ -134,3 +134,24 @@ ggsave(archivo, p1, width = 7, height = 6.7, dpi = 150)
 Dat %>%
   filter(RESULTADO == "1") %>%
   filter(!is.na(FECHA_DEF))
+
+
+# Centinela <- tibble(semana = 8:13,total_usmer = c(43329, 43113, 45420, 42072, 48679, 33500),
+#                     posibles_usmer = c(2472, 2597, 2465, 2919, 4081,5215),
+#                     pruebas_usmer = c(961, 1034, 991, 1423, 2360, 3181),
+#                     totales_nacional = c(583545, 561882,
+#                                          609935, 550483,
+#                                          652335, 488071),
+#                     posibles_nacional = c(42259, 43534,
+#                                           40680, 48186,
+#                                           69718, 90678),
+#                     positivos_usmer = c(3,3,27,167,295,544),
+#                     estimados = c(133,131,751,4718,7566,13221))
+# Centinela <- Centinela %>%
+#   mutate(fecha = "2019-12-29" %>% as.Date(format = "%Y-%m-%d")) %>%
+#   mutate(fecha = fecha + 7*(semana),
+#          casos_acumulados = cumsum(estimados)) %>%
+#   transmute(fecha,
+#             casos_nuevos = casos_acumulados - lag(casos_acumulados, 1, default = 0),
+#             casos_acumulados,
+#             dia = as.numeric(fecha - min(fecha)))
