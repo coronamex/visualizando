19480a1b7bf60b24f1552a01f07b6ff4da4ed307
surh <sur00mx@gmail.com>
2020-04-26 18:47:35 -0700

    Actualización 2020-04-26

---
+++ analisis_diarios_lentos.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+Rscript casos/sir.r
+if [ $? -ne 0 ]; then
+    echo "Error"
+    exit 1
+fi
+Rscript casos/centinela_preliminar.r
+if [ $? -ne 0 ]; then
+    echo "Error"
+    exit 1
+fi
+
+jupyter nbconvert --to script --execute --ExecutePreprocessor.timeout=-1 estados/rt.live.ipynb
+if [ $? -ne 0 ]; then
+    echo "Error"
+    exit 1
+fi
+Rscript estados/r_efectiva.r
+if [ $? -ne 0 ]; then
+    echo "Error"
+    exit 1
+fi
---
+++ analisis_diarios_rapidos.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+
+Rscript casos/inicio_sintomas_por_fecha.r
+if [ $? -ne 0 ]; then
+    echo "Error"
+    exit 1
+fi
+
+Rscript clinicos/riesgos_relativos.r
+if [ $? -ne 0 ]; then
+    echo "Error"
+    exit 1
+fi
+Rscript clinicos/tiempo_sintomas_muerte.r
+if [ $? -ne 0 ]; then
+    echo "Error"
+    exit 1
+fi
+
+Rscript estados/estados_casos100mil.r
+if [ $? -ne 0 ]; then
+    echo "Error"
+    exit 1
+fi
+Rscript estados/estados_muertes_recientes.r
+if [ $? -ne 0 ]; then
+    echo "Error"
+    exit 1
+fi
+
+Rscript muertes/letalidad_mexico.r
+if [ $? -ne 0 ]; then
+    echo "Error"
+    exit 1
+fi
+
+Rscript mundo/confirmados_muertes_por_dia.r
+if [ $? -ne 0 ]; then
+    echo "Error"
+    exit 1
+fi
+
+Rscript municipios/muertes_recientes_municipios.r
+if [ $? -ne 0 ]; then
+    echo "Error"
+    exit 1
+fi
+Rscript municipios/n_municipios.r
+if [ $? -ne 0 ]; then
+    echo "Error"
+    exit 1
+fi
