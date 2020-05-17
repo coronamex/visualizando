43185494e3f8f6cf929bd7faddd31fac239fecc2
surh <sur00mx@gmail.com>
2020-05-16 17:59:00 -0700

    Actalización 2020-05-16

--- estados/r_efectiva.r
+++ estados/r_efectiva.r
@@ -13,12 +13,13 @@ p1 <- Dat %>%
   geom_line(size = 2, col = "blue") +
   geom_hline(yintercept = 1) +
   ylab("Promedio de contagios por enfermo de COVID-19 (R_t)") +
-  xlab("Fecha") +
+  # xlab("Fecha") +
   # AMOR::theme_blackbox() +
   theme(panel.background = element_blank(),
         axis.text = element_text(size = 10),
         axis.text.x = element_text(angle = 90),
         axis.ticks.x = element_blank(),
+        axis.title.x = element_blank(),
         legend.position = "top",
         axis.title = element_text(face = "bold", size = 12),
         plot.margin = margin(l = 20, r = 20, b = 20),
@@ -29,9 +30,9 @@ p1 <- Dat %>%
 # archivo <- file.path(args$dir_salida, "r_efectiva@2x.png")
 # ggsave(archivo, p1, width = 7, height = 6.7, dpi = 150)
 archivo <- file.path(args$dir_salida, "r_efectiva.png")
-ggsave(archivo, p1, width = 7, height = 9, dpi = 75)
+ggsave(archivo, p1, width = 7, height = 9.5, dpi = 75)
 archivo <- file.path(args$dir_salida, "r_efectiva@2x.png")
-ggsave(archivo, p1, width = 7, height = 9, dpi = 150)
+ggsave(archivo, p1, width = 7, height = 9.5, dpi = 150)
 
 
 
