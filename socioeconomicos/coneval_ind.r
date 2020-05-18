61ed9aed7c19b1c226dc3aeeb5f6b143b6170952
surh <sur00mx@gmail.com>
2020-05-18 16:52:19 -0700

    Seleccionando indicadores
    Intenté modelar de otras maneras pero ninguna funcionó tan claramente.
    Modelar por número de casos no controla correctametne la población

--- socioeconomicos/coneval_ind.r
+++ socioeconomicos/coneval_ind.r
@@ -60,7 +60,6 @@ Dens <- read_csv(args$densidad,
                                                  .default = col_number()))
 Dens <- Dens %>%
   select(NOM_MUN, CVMUN, POBTOT15, AREAKM, DENS15)
-# Dens
 
 # Leer indicadores
 Ind <- read_csv(args$indicadores,
@@ -74,7 +73,6 @@ Ind <- Ind %>%
   select(!ends_with("_pob")) %>%
   rename(pv = npnv) %>%
   mutate(pv = 100 - pv)
-# Ind 
 
 # Leer datos
 Casos <- read_csv(args$serie_municipios)
@@ -86,7 +84,6 @@ Casos <- Casos %>%
             dia_10 = min(fecha[ sintomas_acumulados >= 10])) %>%
   filter(casos_totales >= 10) %>%
   mutate(brote_dias = as.numeric(dia_10 - dia_1))
-Casos
 
 # Unir densidad, indicadores y pruebas
 Dat <- Dens %>%
@@ -100,11 +97,13 @@ Dat <- Dens %>%
   mutate(clave_entidad = str_pad(string = clave_entidad, width = 2, side = "left", pad = "0")) %>%
   mutate(clave = paste(clave_entidad, mun, sep = "_")) %>%
   select(clave, everything(), -clave_entidad, -clave_municipio, -mun)
-# Dat
 
 # Unir datos COVID19 con indicadores
 Dat <- Casos %>%
   left_join(Dat, by = "clave") %>%
+  # mutate(incidencia = casos_totales,
+  #        mortalidad = muertes_totales,
+  #        letalidad = (muertes_totales)/ (casos_totales))
   mutate(incidencia = 1e5 * (casos_totales) / (poblacion),
          mortalidad = 1e5 * (muertes_totales) / (poblacion),
          letalidad = (muertes_totales)/ (casos_totales))
@@ -113,30 +112,57 @@ Dat <- Casos %>%
   #        letalidad = (muertes_totales + 1)/ (casos_totales + 1))
 # Dat
 
-Dat %>%
+p1 <- Dat %>%
   select(clave, casos_totales, poblacion, DENS15, n_pruebas) %>%
-  pivot_longer(cols = c(-clave, -casos_totales), names_to = "indicador", values_to = "valor") %>%
+  pivot_longer(cols = c(-clave, -casos_totales),
+               names_to = "indicador", values_to = "valor") %>%
+  # select(clave, brote_dias, poblacion, DENS15, n_pruebas) %>%
+  # pivot_longer(cols = c(-clave, -brote_dias),
+  #              names_to = "indicador", values_to = "valor") %>%
+  mutate(indicador = replace(indicador, indicador == "poblacion", "Población total")) %>%
+  mutate(indicador = replace(indicador, indicador == "DENS15", "Densidad poblacional")) %>%
+  mutate(indicador = replace(indicador, indicador == "n_pruebas", "Pruebas totales")) %>%
   ggplot(aes(x = valor, y = casos_totales)) +
-  facet_wrap(~ indicador, scales = "free_x") +
+  facet_wrap(~ indicador, scales = "free_x", nrow = 2) +
   geom_point() +
-  scale_x_log10() +
-  scale_y_log10()
-
+  scale_x_log10(labels = function(x) scales::comma(x, accuracy = 1)) +
+  scale_y_log10(labels = function(x) scales::comma(x, accuracy = 1)) +
+  ylab(label = "Casos totales por municipio") +
+  # AMOR::theme_blackbox() +
+  theme_classic() +
+  theme(axis.title = element_text(size = 20),
+        axis.title.x = element_blank(),
+        axis.text = element_text(size = 10),
+        plot.margin = margin(l = 20, r = 20),
+        panel.background = element_blank(),
+        # panel.border = element_rect(fill=NA, colour = "black", size = 3),
+        legend.position = "top",
+        legend.text = element_text(size = 12),
+        legend.background = element_blank(),
+        strip.text = element_text(face = "bold"))
+p1
+ggsave("test.png", p1, width = 7, height = 6.7, dpi = 75)
 
 # Calcular residuales de datos COVID-19 controlados por población
 variables <- c("incidencia", "mortalidad", "letalidad")
 Dat <- bind_cols(Dat,
-                 variables %>%
-                   map_dfc(function(var, dat = Dat){
-                     var <- ensym(var)
-                     dat_filtrada <- dat %>% filter(!!var > 0)
-                     new_var = paste0("resid_", var)
-                     f1 <- paste("log10(", var, ") ~ log10(poblacion) + log10(DENS15) + log10(n_pruebas)")
-                     lm(f1, data = dat_filtrada) %>%
-                       augment(newdata = dat) %>%
-                       transmute(!!new_var := log10(!!var) - .fitted)
-                   }))
-# Dat
+          variables %>%
+            map_dfc(function(var, dat = Dat){
+              var <- ensym(var)
+              dat_filtrada <- dat %>% filter(!!var > 0)
+              new_var = paste0("resid_", var)
+              f1 <- paste("log10(", var, ") ~ log10(poblacion) + log10(DENS15) + log10(n_pruebas)")
+              lm(f1, data = dat_filtrada) %>%
+                augment(newdata = dat) %>%
+                # transmute(!!new_var := !!var - 10^(.fitted))
+                transmute(!!new_var := log10(!!var) - .fitted)
+            }))
+# summary(Dat$resid_mortalidad)
+# summary(Dat$resid_letalidad)
+# summary(Dat$resid_incidencia)
+# hist(Dat$resid_mortalidad)
+# hist(Dat$resid_letalidad)
+# hist(Dat$resid_incidencia)
 
 # Limpiar para análisis de indicadores
 Dat <- Dat %>%
@@ -146,26 +172,29 @@ Dat <- Dat %>%
                         -poblacion, -DENS15, -n_pruebas,
                         -resid_incidencia, -resid_mortalidad, -resid_letalidad),
                names_to = "indicador", values_to = "valor")
-# Dat
+Dat
 
+indicadores_para_graficar <- c("pobreza", "ic_asalud", "ic_cv", "ic_segsoc")
 p1 <- Dat %>%
+  filter(indicador %in% indicadores_para_graficar) %>%
   ggplot(aes(x = valor, y = resid_incidencia)) +
   facet_wrap(~ indicador, scales = "free_x") +
-  # facet_wrap(~ indicador) +
-  geom_point(aes(col = poblacion / 1e4), size = 0.5) +
+  geom_point(aes(col = poblacion / 1e4), size = 2) +
   scale_color_gradient(low = "#f6e8c3", high = "#543005", trans = "log10",
                        name = "Población\n(decenas de miles)", labels = scales::comma) +
   # scale_radius(trans = "log2") +
   stat_smooth(method = "lm") +
   scale_x_log10(labels = function(x) scales::percent(x / 100)) +
   # scale_x_continuous(labels = function(x) scales::percent(x / 100)) +
+  # scale_y_log10() +
   scale_y_continuous(labels = function(x){
     # labs <- (10 ^ x) - 1
     # scales::percent(labs)
     labs <- (10 ^ x)
     scales::number(labs, accuracy = 0.01)
   }) +
-  coord_cartesian() +
+  ylab(expression(frac("Casos totales observados","Casos totatels esperados"))) +
+  # coord_cartesian() +
   theme_classic() +
   theme(legend.position = "top",
         axis.text.x = element_text(angle = 90))
@@ -176,17 +205,24 @@ ggsave("test.png", p1, width = 7, height = 9, dpi = 75)
 # archivo <- file.path(args$dir_salida, "inicio_sintomas_por_fecha_nacional@2x.png")
 # ggsave(archivo, p1, width = 7, height = 6.7, dpi = 150)
  
-Dat %>%
-  ggplot(aes(x = valor, y = resid_mortalidad)) +
-  facet_wrap(~ indicador, scales = "free_x") +
-  geom_point() +
-  stat_smooth(method = "lm") +
-  scale_x_log10() +
-  scale_x_log10(labels = function(x) scales::percent(x / 100)) +
-  scale_y_continuous(labels = function(x){
-    labs <- (10 ^ x) - 1
-    scales::percent(labs)
-  })
+# Dat %>%
+#   filter(indicador %in% indicadores_para_graficar) %>%
+#   ggplot(aes(x = valor, y = resid_mortalidad)) +
+#   facet_wrap(~ indicador, scales = "free_x") +
+#   geom_point(aes(col = poblacion / 1e4), size = 0.5) +
+#   scale_color_gradient(low = "#f6e8c3", high = "#543005", trans = "log10",
+#                        name = "Población\n(decenas de miles)", labels = scales::comma) +
+#   stat_smooth(method = "lm") +
+#   scale_x_log10(labels = function(x) scales::percent(x / 100)) +
+#   scale_y_continuous(labels = function(x){
+#     labs <- (10 ^ x)
+#     scales::number(labs, accuracy = 0.01)
+#   }) +
+#   theme_classic() +
+#   theme(legend.position = "top",
+#         axis.text.x = element_text(angle = 90))
+
+
 
 # Dat %>%
 #   ggplot(aes(x = valor, y = resid_letalidad)) +
@@ -216,11 +252,11 @@ Dat %>%
   facet_wrap(~ indicador, scales = "free_x") +
   geom_point() +
   stat_smooth(method = "lm") +
-  scale_x_log10(labels = function(x) scales::percent(x / 100)) +
-  scale_y_continuous(labels = function(x){
-    labs <- (10 ^ x) - 1
-    scales::percent(labs)
-  })
+  scale_x_log10(labels = function(x) scales::percent(x / 100))
+  # scale_y_continuous(labels = function(x){
+  #   labs <- (10 ^ x) - 1
+  #   scales::percent(labs)
+  # })
 
 # Dat %>%
 #   ggplot(aes(x = valor, y = resid_letalidad - resid_incidencia)) +
@@ -240,3 +276,9 @@ modelar(resid_letalidad, Dat = Dat)
 modelar(resid_mortalidad - resid_incidencia, Dat = Dat)
 modelar(resid_letalidad - resid_mortalidad, Dat = Dat)
 modelar(resid_letalidad - resid_incidencia, Dat = Dat)
+
+################
+# library(geojsonio)
+# 
+# mun_sp <- geojson_read("../socioeconomicos/mapas/municipios.json")
+# mun_sp
