8a53777dfcbcec32bd1913439f18f764a2bba340
surh <sur00mx@gmail.com>
2020-05-18 20:41:26 -0700

    Usando estimados previos

--- socioeconomicos/coneval_ind.r
+++ socioeconomicos/coneval_ind.r
@@ -43,30 +43,32 @@ args <- list(base_de_datos = "../datos/datos_abiertos/base_de_datos.csv",
              indicadores = "../socioeconomicos/coneval/coneval_indicadores_pobreza_municipa_2015.csv",
              serie_municipios = "../datos/datos_abiertos/serie_tiempo_municipio_res_confirmados.csv",
              mapa_shp = "../socioeconomicos/mapas/municipalities.shp",
+             datos_municipios = "estimados/municipios_obs_esp.csv",
              min_casos = 0)
 
 # Leer mapa
 mun_sp <- geojson_read(args$mapa_shp, what = "sp")
 mun_sp@data$clave <- paste(mun_sp@data$CVE_ENT, mun_sp@data$CVE_MUN, sep = "_")
+mun_sp <- tidy(mun_sp, region = "clave")
 
-# Número de pruebas por municipio
-n_pruebas <- leer_datos_abiertos(archivo = args$base_de_datos, solo_confirmados = FALSE, solo_fallecidos = FALSE)
-n_pruebas <- n_pruebas %>%
-  filter(RESULTADO %in% c("1", "2")) %>%
-  group_by(ENTIDAD_RES, MUNICIPIO_RES) %>%
-  summarise(n_pruebas = length(ID_REGISTRO)) %>%
-  ungroup() %>%
-  mutate(clave_municipio = paste(ENTIDAD_RES, MUNICIPIO_RES, sep = "")) %>%
-  select(clave_municipio, n_pruebas) %>%
-  mutate(clave_municipio = str_remove(clave_municipio, "^0"))
-  
-# Leer densidad
-Dens <- read_csv(args$densidad,
-                 col_types = cols(NOM_MUN = col_character(),
-                                                 CVMUN = col_character(),
-                                                 .default = col_number()))
-Dens <- Dens %>%
-  select(NOM_MUN, CVMUN, POBTOT15, AREAKM, DENS15)
+# # Número de pruebas por municipio
+# n_pruebas <- leer_datos_abiertos(archivo = args$base_de_datos, solo_confirmados = FALSE, solo_fallecidos = FALSE)
+# n_pruebas <- n_pruebas %>%
+#   filter(RESULTADO %in% c("1", "2")) %>%
+#   group_by(ENTIDAD_RES, MUNICIPIO_RES) %>%
+#   summarise(n_pruebas = length(ID_REGISTRO)) %>%
+#   ungroup() %>%
+#   mutate(clave_municipio = paste(ENTIDAD_RES, MUNICIPIO_RES, sep = "")) %>%
+#   select(clave_municipio, n_pruebas) %>%
+#   mutate(clave_municipio = str_remove(clave_municipio, "^0"))
+#   
+# # Leer densidad
+# Dens <- read_csv(args$densidad,
+#                  col_types = cols(NOM_MUN = col_character(),
+#                                                  CVMUN = col_character(),
+#                                                  .default = col_number()))
+# Dens <- Dens %>%
+#   select(NOM_MUN, CVMUN, POBTOT15, AREAKM, DENS15)
 
 # Leer indicadores
 Ind <- read_csv(args$indicadores,
@@ -77,106 +79,64 @@ Ind <- read_csv(args$indicadores,
                                  .default = col_number()),
                 na = c("", "NA", "n.d"))
 Ind <- Ind %>%
-  select(!ends_with("_pob")) %>%
+  select(!ends_with("_pob"), -entidad_federativa, - municipio, -poblacion) %>%
   rename(pv = npnv) %>%
-  mutate(pv = 100 - pv)
-
-# Leer datos
-Casos <- read_csv(args$serie_municipios)
-Casos <- Casos %>%
-  group_by(clave) %>%
-  summarise(casos_totales = sum(sintomas_nuevos),
-            muertes_totales = sum(muertes_nuevas),
-            dia_1 = min(fecha[ sintomas_acumulados >= 1]),
-            dia_10 = min(fecha[ sintomas_acumulados >= 10])) %>%
-  filter(casos_totales >= args$min_casos) %>%
-  mutate(brote_dias = as.numeric(dia_10 - dia_1))
-
-# Unir densidad, indicadores y pruebas
-Dat <- Dens %>%
-  mutate(CVMUN = str_remove(string = CVMUN, pattern = "^0")) %>%
-  rename(clave_municipio = CVMUN) %>%
-  full_join(Ind, by = "clave_municipio") %>%
-  full_join(n_pruebas, by = "clave_municipio") %>%
-  select(-POBTOT15, -AREAKM,
-         -NOM_MUN, -municipio, -entidad_federativa) %>%
+  mutate(pv = 100 - pv) %>% 
   mutate(mun = str_sub(clave_municipio, start = -3)) %>%
   mutate(clave_entidad = str_pad(string = clave_entidad, width = 2, side = "left", pad = "0")) %>%
   mutate(clave = paste(clave_entidad, mun, sep = "_")) %>%
   select(clave, everything(), -clave_entidad, -clave_municipio, -mun)
 
-# Unir datos COVID19 con indicadores
+# # Leer datos
+# Casos <- read_csv(args$serie_municipios)
+# Casos <- Casos %>%
+#   group_by(clave) %>%
+#   summarise(casos_totales = sum(sintomas_nuevos),
+#             muertes_totales = sum(muertes_nuevas),
+#             dia_1 = min(fecha[ sintomas_acumulados >= 1]),
+#             dia_10 = min(fecha[ sintomas_acumulados >= 10])) %>%
+#   filter(casos_totales >= args$min_casos) %>%
+#   mutate(brote_dias = as.numeric(dia_10 - dia_1))
+Casos <- read_csv(args$datos_municipios)
+Casos
+
 Dat <- Casos %>%
-  left_join(Dat, by = "clave") %>%
-  # mutate(incidencia = casos_totales,
-  #        mortalidad = muertes_totales,
-  #        letalidad = (muertes_totales)/ (casos_totales))
-  mutate(incidencia = 1e5 * (casos_totales) / (poblacion),
-         mortalidad = 1e5 * (muertes_totales) / (poblacion),
-         letalidad = (muertes_totales)/ (casos_totales))
+  left_join(Ind, by = "clave")
+
+# datos e indicadores
+# Dat <- Dens %>%
+#   mutate(CVMUN = str_remove(string = CVMUN, pattern = "^0")) %>%
+#   rename(clave_municipio = CVMUN) %>%
+#   full_join(Ind, by = "clave_municipio") %>%
+#   full_join(n_pruebas, by = "clave_municipio") %>%
+#   select(-POBTOT15, -AREAKM,
+#          -NOM_MUN, -municipio, -entidad_federativa) %>%
+#   mutate(mun = str_sub(clave_municipio, start = -3)) %>%
+#   mutate(clave_entidad = str_pad(string = clave_entidad, width = 2, side = "left", pad = "0")) %>%
+#   mutate(clave = paste(clave_entidad, mun, sep = "_")) %>%
+#   select(clave, everything(), -clave_entidad, -clave_municipio, -mun)
+# 
+# # Unir datos COVID19 con indicadores
+# Dat <- Casos %>%
+#   left_join(Dat, by = "clave") %>%
+#   # mutate(incidencia = casos_totales,
+#   #        mortalidad = muertes_totales,
+#   #        letalidad = (muertes_totales)/ (casos_totales))
+#   mutate(incidencia = 1e5 * (casos_totales) / (poblacion),
+#          mortalidad = 1e5 * (muertes_totales) / (poblacion),
+#          letalidad = (muertes_totales)/ (casos_totales))
   
 # mutate(incidencia = 1e5 * (casos_totales + 1) / (poblacion + 1),
   #        mortalidad = 1e5 * (muertes_totales + 1) / (poblacion + 1),
   #        letalidad = (muertes_totales + 1)/ (casos_totales + 1))
 # Dat
 
-p1 <- Dat %>%
-  select(clave, casos_totales, poblacion, DENS15, n_pruebas) %>%
-  pivot_longer(cols = c(-clave, -casos_totales),
-               names_to = "indicador", values_to = "valor") %>%
-  # select(clave, brote_dias, poblacion, DENS15, n_pruebas) %>%
-  # pivot_longer(cols = c(-clave, -brote_dias),
-  #              names_to = "indicador", values_to = "valor") %>%
-  mutate(indicador = replace(indicador, indicador == "poblacion", "Población total")) %>%
-  mutate(indicador = replace(indicador, indicador == "DENS15", "Densidad poblacional")) %>%
-  mutate(indicador = replace(indicador, indicador == "n_pruebas", "Pruebas totales")) %>%
-  ggplot(aes(x = valor, y = casos_totales)) +
-  facet_wrap(~ indicador, scales = "free_x", nrow = 2) +
-  geom_point() +
-  scale_x_log10(labels = function(x) scales::comma(x, accuracy = 1)) +
-  scale_y_log10(labels = function(x) scales::comma(x, accuracy = 1)) +
-  ylab(label = "Casos totales por municipio") +
-  # AMOR::theme_blackbox() +
-  theme_classic() +
-  theme(axis.title = element_text(size = 20),
-        axis.title.x = element_blank(),
-        axis.text = element_text(size = 10),
-        plot.margin = margin(l = 20, r = 20),
-        panel.background = element_blank(),
-        # panel.border = element_rect(fill=NA, colour = "black", size = 3),
-        legend.position = "top",
-        legend.text = element_text(size = 12),
-        legend.background = element_blank(),
-        strip.text = element_text(face = "bold"))
-p1
-ggsave("test.png", p1, width = 7, height = 6.7, dpi = 75)
-
-# Calcular residuales de datos COVID-19 controlados por población
-variables <- c("incidencia", "mortalidad", "letalidad")
-Dat <- bind_cols(Dat,
-          variables %>%
-            map_dfc(function(var, dat = Dat){
-              var <- ensym(var)
-              dat_filtrada <- dat %>% filter(!!var > 0)
-              new_var = paste0("resid_", var)
-              f1 <- paste("log10(", var, ") ~ log10(poblacion) + log10(DENS15) + log10(n_pruebas)")
-              lm(f1, data = dat_filtrada) %>%
-                augment(newdata = dat) %>%
-                # transmute(!!new_var := !!var - 10^(.fitted))
-                transmute(!!new_var := log10(!!var) - .fitted)
-            }))
-# summary(Dat$resid_mortalidad)
-# summary(Dat$resid_letalidad)
-# summary(Dat$resid_incidencia)
-# hist(Dat$resid_mortalidad)
-# hist(Dat$resid_letalidad)
-# hist(Dat$resid_incidencia)
 Dat
+
 # Mapas
-mun_sp <- tidy(mun_sp, region = "clave") %>%
+mun_sp <- mun_sp %>%
   left_join(Dat %>%
-              select(clave, casos_totales, muertes_totales, brote_dias,
-                     resid_incidencia, resid_mortalidad, resid_letalidad),
+              select(clave, resid_incidencia),
             by = c(id = "clave"))
 mun_sp
 p1 <- mun_sp %>%
