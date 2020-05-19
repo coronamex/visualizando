990081e060032e7478cf513e3b08ea6ba7fa3b3b
surh <sur00mx@gmail.com>
2020-05-19 00:56:05 -0700

    copyright y etiquetas

--- socioeconomicos/coneval_ind.r
+++ socioeconomicos/coneval_ind.r
@@ -1,3 +1,18 @@
+# (C) Copyright 2020 Sur Herrera Paredes
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 library(tidyverse)
 library(broom)
 library(geojsonio)
@@ -107,7 +122,7 @@ Dat <- Dat %>%
 
 # indicadores_para_graficar <- c("pobreza", "ic_asalud", "ic_cv", "ic_segsoc")
 indicadores_para_graficar <- c(pobreza_e = "Pobreza extrema",
-                               vul_ing = "Vulnerable por ingreso",
+                               vul_ing = "Ingreso menor a línea de bienestar",
                                ic_cv = "Vulnerabale por calidad y espacios de la vivienda",
                                carencias3 = "3 ó más carencias sociales")
 # indicadores_para_graficar <- unique(Dat$indicador)
