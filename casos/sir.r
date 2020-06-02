559d80a62a14ebc43ae5fe23a84e7bf0df918213
surh <sur00mx@gmail.com>
2020-06-02 10:28:28 -0700

    Suavizando datos y usando sólo SANN

--- casos/sir.r
+++ casos/sir.r
@@ -16,7 +16,7 @@
 library(tidyverse)
 library(deSolve)
 source("casos/sir_funciones.r")
-source("util/leer_datos_abiertos.r")
+# source("util/leer_datos_abiertos.r")
 # https://rstudio-pubs-static.s3.amazonaws.com/6852_c59c5a2e8ea3456abbeb017185de603e.html
 # https://gabgoh.github.io/COVID/index.html
 # https://en.wikipedia.org/wiki/Compartmental_models_in_epidemiology
@@ -45,8 +45,14 @@ Tab <- Tab %>%
   select(fecha, casos_nuevos = sintomas_nuevos,
          casos_acumulados = sintomas_acumulados) %>%
   arrange(fecha) %>%
-  filter(casos_acumulados >= 5 & casos_nuevos > 0) %>%
+  mutate(casos_nuevos = floor(zoo::rollmean(casos_nuevos, k = 7, fill = NA, align = "center"))) %>%
+  # print(n = 200)
+  filter(casos_nuevos > 0) %>%
+  # print(n = 30)
   mutate(dia = as.numeric(fecha - min(fecha)))
+
+
+
 
 # Precalcular dias
 fecha_inicio <- min(Tab$fecha)
@@ -58,26 +64,32 @@ n_dias_ajuste <- n_dias - args$dias_retraso + 1
 # fechas <- c("2020-03-02", "2020-03-09","2020-03-16", "2020-03-23", "2020-03-30") %>% parse_date(format = "%Y-%m-%d")
 # fechas <- c("2020-03-01","2020-03-08", "2020-03-15", "2020-03-22", "2020-03-29", "2020-04-05") %>%
 #   parse_date(format = "%Y-%m-%d")
+# fechas <- c("2020-03-01", "2020-03-15", "2020-04-15", "2020-05-10") %>%
+#   parse_date(format = "%Y-%m-%d")
 # fechas_dias <- as.numeric(fechas - fecha_inicio)
-fechas_dias <- sort(n_dias_ajuste - seq(from = 10, by = 11, length.out = 6))
-# fechas_dias
+fechas_dias <- sort(n_dias_ajuste - seq(from = 10, by = 15, length.out = 5))
+# fechas_dias <- seq(from = 15, by = 15, length.out = 6)
+fechas_dias
 
 # Parameters to make optimization
 # pob <- 135552447
 T_inc <- c(4, 5, 6)
-T_inf <- c(2, 3, 4)
+# T_inf <- c(2, 3, 4)
+T_inf <- c(5, 6, 7)
 # T_inc <- c(4, 5)
-# T_inf <- c(2)   
+# T_inf <- c(2)
 pob <- 127792286
-# T_inc <- c(5,5.2,5.4)
-# T_inf <- c(2.5,3,3.5)
-# T_inc <- 5
-# T_inf <- 3
+# T_inc <- 4
+# T_inf <- 4
 
 R_hat <- encontrar_R_0(real = Tab, n_dias_ajuste = n_dias_ajuste,
                        dias_int = fechas_dias,
                        T_inc = T_inc, T_inf = T_inf, pob = pob)
+# R_ha1 <- R_hat
 R_hat
+# save(R_hat, file = "smooth.rdat")
+# save(R_hat, file = "r_hat_sir_7x15_lbfgsb.rdat")
+# save(R_hat, file = "r_hat_sir_7x15_sann.rdat")
 save(R_hat, file = "R_hat_coronamex.rdat")
 # load("R_hat_coronamex.rdat")
 
@@ -175,7 +187,7 @@ sims_parciales <- R_hat %>%
 
 
       Ms <- list()
-      for(i in c(2, 4, 6)){
+      for(i in c(2, 3, 5)){
         m_parcial <- l
         m_parcial$tiempos_int <- m_parcial$tiempos_int[1:i]
         m_parcial$efectos_int <- m_parcial$efectos_int[1:i]
@@ -200,23 +212,27 @@ p1 <- Tab %>%
   select(fecha, dia, casos_acumulados) %>%
   mutate(modelo = "real") %>%
   bind_rows(sims_parciales %>% mutate(fecha = fecha_inicio + dia)) %>%
+  # select(modelo) %>% table
+  # filter(str_detect(modelo, "[.]7$"))
   mutate(grupo = "¿Qué pudo haber pasado?") %>%
-  mutate(grupo =  replace(grupo, modelo %>% str_detect(pattern = "[.]6$"), "¿Qué creemos que está pasando?")) %>%
+  mutate(grupo =  replace(grupo, modelo %>% str_detect(pattern = "[.]5$"), "¿Qué creemos que está pasando?")) %>%
   mutate(grupo = replace(grupo,
                          modelo %>% str_detect(pattern = "[.]2$"),
                          paste("¿Qué pudo haber pasado?\n(hace", n_dias - fechas_dias[2], "días)"))) %>%
   mutate(grupo = replace(grupo,
-                         modelo %>% str_detect(pattern = "[.]4$"),
-                         paste("¿Qué pudo haber pasado?\n(hace", n_dias - fechas_dias[4], "días)"))) %>%
+                         modelo %>% str_detect(pattern = "[.]3$"),
+                         paste("¿Qué pudo haber pasado?\n(hace", n_dias - fechas_dias[3], "días)"))) %>%
   mutate(grupo = replace(grupo, modelo == "real", "Inicio de síntomas")) %>%
   mutate(grupo = factor(grupo, levels = c("Inicio de síntomas",
                                           paste("¿Qué pudo haber pasado?\n(hace", n_dias - fechas_dias[2], "días)"),
-                                          paste("¿Qué pudo haber pasado?\n(hace", n_dias - fechas_dias[4], "días)"),
+                                          paste("¿Qué pudo haber pasado?\n(hace", n_dias - fechas_dias[3], "días)"),
                                           "¿Qué creemos que está pasando?"))) %>%
   mutate(modelo = factor(modelo, levels = c("real", unique(sims_parciales$modelo)))) %>%
+  # filter(str_detect(modelo, "[.]7$")) %>% print(n = 1000)
 
   filter(casos_acumulados <= max_estimado_actual) %>%
   
+  # arrange(casos_acumulados)
   # filter(fecha >= "2020-02-15") %>%
   # filter(modelo == "real") %>%
   # filter(dia < 75) %>%
@@ -267,114 +283,28 @@ ggsave(archivo, p1, width = 7, height = 6.7, dpi = 150)
 
 ##### R_hat
 
-p1 <- R_hat %>%
-  map_dfr(~ tibble(R0 = c(.x$R_0, .x$R_0 * .x$efectos_int),
-               dias = c(0, fechas_dias),
-               modelo = .x$modelo)) %>%
-  mutate(fecha = fecha_inicio + dias) %>%
-  
-  ggplot(aes(x = fecha, y = R0, group = modelo)) +
-  geom_line() +
-  ylab("Promedio de infectados por enfermo (R_t)") +
-  xlab("Fecha") +
-  AMOR::theme_blackbox() +
-  theme(panel.background = element_blank(),
-        panel.border = element_rect(fill = NA, color = "black", size = 3),
-        legend.position = "top",
-        legend.text = element_text(size = 14),
-        legend.key = element_blank(),
-        axis.title = element_text(size = 20),
-        axis.text = element_text(size = 10, color = "black"),
-        plot.margin = margin(l = 20, r = 20))
+# p1 <- R_hat %>%
+#   map_dfr(~ tibble(R0 = c(.x$R_0, .x$R_0 * .x$efectos_int),
+#                dias = c(0, fechas_dias),
+#                modelo = .x$modelo)) %>%
+#   mutate(fecha = fecha_inicio + dias) %>%
+#   
+#   ggplot(aes(x = fecha, y = R0, group = modelo)) +
+#   geom_line() +
+#   ylab("Promedio de infectados por enfermo (R_t)") +
+#   xlab("Fecha") +
+#   AMOR::theme_blackbox() +
+#   theme(panel.background = element_blank(),
+#         panel.border = element_rect(fill = NA, color = "black", size = 3),
+#         legend.position = "top",
+#         legend.text = element_text(size = 14),
+#         legend.key = element_blank(),
+#         axis.title = element_text(size = 20),
+#         axis.text = element_text(size = 10, color = "black"),
+#         plot.margin = margin(l = 20, r = 20))
 # p1
 # ggsave("test.png", p1, width = 7, height = 6.7, dpi = 150)
-archivo <- file.path(args$dir_salida, "sir_nacional_r0.png")
-ggsave(archivo, p1, width = 7, height = 6.7, dpi = 75)
-archivo <- file.path(args$dir_salida, "sir_nacional_r0@2x.png")
-ggsave(archivo, p1, width = 7, height = 6.7, dpi = 150)
-
-# dgamma_otimizable <- function(x, dat, dias_cambio = 10){
-#   # dat <- d
-#   # x <- c(4.2, 1.32)
-#   
-#   dat$efectos_norm <- dat$efectos / sum(dat$efectos)
-#   
-#   gamma_x <- (dat$dias / dias_cambio) + 1
-#   # gamma_x <- dat$dias + 1
-#   # gamma_x <- gamma_x / max(gamma_x)
-#   
-#   dat$pred <- dgamma(x = gamma_x, shape = x[1], rate = x[2])
-#   
-#   # dat <- dat %>% filter(dias > 0)
-#   ss <- sum((dat$efectos_norm - dat$pred) ^ 2)
-#   # ss <- sum(log(dat$pred / dat$efectos_norm) ^ 2)
-#   
-#   ss
-# }
-# 
-# proyectar_R_t <- function(l, n_dias = 365, dias_cambio = 10){
-#   # n_dias <- dias_proyeccion
-#   # dias_cambio <- 10
-#   # l <- R_hat[[1]]
-#   
-#   # Crear tibble con datos
-#   d <- tibble(dias = c(0, l$tiempos_int),
-#               efectos = c(l$R_0, l$R_0 * l$efectos_int))
-#   # d <- tibble(dias = c(l$tiempos_int),
-#   #             efectos = c(l$R_0 * l$efectos_int))
-#   # d <- tibble(dias = c(0, 10, 20, 30, 40),
-#   #             efectos = c(2, 4, 2.5, 1.8, 1.4))
-#   # d
-#   
-#   # m1 <- MASS::fitdistr(x = d$efectos / sum(d$efectos), densfun = "gamma")
-#   # m1 <- MASS::fitdistr(x = d$efectos / sum(d$efectos), densfun = "gamma")
-#   # m1
-#   ajuste_R_0 <- nlm(f = dgamma_otimizable , p = c(4,2), dat = d, dias_cambio = dias_cambio)
-#   # ajuste_R_0 <- nlm(f = dgamma_otimizable , p = c(80,30), dat = d, dias_cambio = dias_cambio)
-#   # ajuste_R_0
-#   
-#   dias_nuevos_R_0 <- seq(from = max(d$dias) + dias_cambio , to = n_dias, by = dias_cambio)
-#   gamma_x <- (dias_nuevos_R_0 / dias_cambio) + 1
-#   # gamma_x <- dias_nuevos_R_0  + 1
-#   # gamma_x <- gamma_x / max((d$dias / dias_cambio) + 1)
-#   R_0_proyectadas <- dgamma(x = gamma_x, 
-#                             shape = ajuste_R_0$estimate[1],
-#                             rate = ajuste_R_0$estimate[2]) * sum(d$efectos)
-#   # R_0_proyectadas[R_0_proyectadas < 0.8] <- 0.8
-#   plot(d$dias , d$efectos)
-#   # gamma_x <- (d$dias / dias_cambio)
-#   # gamma_x <- d$dias + 1
-#   # plot(d$dias,
-#   #      dgamma(x = gamma_x, shape = ajuste_R_0$estimate[1], rate = ajuste_R_0$estimate[2]) * sum(d$efectos))
-#   
-#   efectos_proyectados <- R_0_proyectadas / l$R_0
-#   efectos_proyectados[ efectos_proyectados < 0.8 ] <- 0.8
-#   l$tiempos_int <- c(l$tiempos_int, dias_nuevos_R_0)
-#   l$efectos_int <- c(l$efectos_int, efectos_proyectados)
-#   
-#   l
-# }
-# 
-# 
-# fecha_proyeccion <- parse_date("2020-07-01", format = "%Y-%m-%d")
-# dias_proyeccion <- as.numeric(fecha_proyeccion - fecha_inicio)
-# 
-# R_proyecciones <- R_hat %>%
-#   map(proyectar_R_t, n_dias = dias_proyeccion, dias_cambio = 10) 
-# R_hat[[1]]$efectos_int
-# R_proyecciones[[1]]$efectos_int
-# R_hat[[1]]$efectos_int * R_hat[[1]]$R_0
-# R_proyecciones[[1]]$R_0 * R_proyecciones[[1]]$efectos_int
-# 
-# 
-# simular_multiples_modelos(modelos = R_proyecciones,
-#                           FUN = sir, real = Tab, pob = pob,
-#                           # n_dias = as.numeric(parse_date("2020-07-01", format = "%Y-%m-%d") - fecha_inicio),
-#                           n_dias = dias_proyeccion) %>%
-#   split(.$modelo) %>%
-#   map_dfr(~.x %>% mutate(casos_nuevos = casos_acumulados - lag(casos_acumulados, 1, default = min(casos_acumulados)))) %>%
-#   mutate(fecha = fecha_inicio + dia) %>%
-#   
-#   ggplot(aes(x = fecha, y = casos_nuevos, group = modelo)) +
-#   geom_line()
-
+# archivo <- file.path(args$dir_salida, "sir_nacional_r0.png")
+# ggsave(archivo, p1, width = 7, height = 6.7, dpi = 75)
+# archivo <- file.path(args$dir_salida, "sir_nacional_r0@2x.png")
+# ggsave(archivo, p1, width = 7, height = 6.7, dpi = 150)
