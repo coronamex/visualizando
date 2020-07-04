6f2f9de644a74a06ce911ebf1689103c9d62f994
surh <sur00mx@gmail.com>
2020-07-04 08:41:20 -0700

    Actualización 2020-07-03

--- clinicos/riesgos_relativos.r
+++ clinicos/riesgos_relativos.r
@@ -1,10 +1,11 @@
 library(tidyverse)
-library(epitools)
+# library(epitools)
+library(lme4)
 source("util/leer_datos_abiertos.r")
 
-logit <- function(x){
-  1 / (1 + exp(-x))
-}
+# logistic <- function(x){
+#   1 / (1 + exp(-x))
+# }
 
 args <- list(dir_salida = "../sitio_hugo/static/imagenes/",
              base_de_datos = "../datos/datos_abiertos/base_de_datos.csv.gz")
@@ -24,19 +25,113 @@ Dat <- leer_datos_abiertos(archivo = args$base_de_datos,
 Dat <- Dat %>%
   filter(FECHA_SINTOMAS < Sys.Date() - 15 | !is.na(FECHA_DEF))
 
+
+################## Regresión logística múltiple multinivel ############
+# names(Dat)
+# 
+# # Seleccionar datos y convertir variables a indicadores
+# d <- Dat %>%
+#   mutate(DEF = 1*(!is.na(FECHA_DEF)),
+#          TIPO_PACIENTE = replace(TIPO_PACIENTE, TIPO_PACIENTE == "99", NA)) %>%
+#   # select(TIPO_PACIENTE) %>% table(useNA = "a")
+#   mutate(HOSP = 1*(TIPO_PACIENTE == "2"))  %>%
+#   select(-FECHA_ACTUALIZACION, -FECHA_INGRESO, -FECHA_SINTOMAS, -FECHA_DEF,
+#          -ORIGEN, -TIPO_PACIENTE,
+#          -ENTIDAD_NAC, -ENTIDAD_RES, -MIGRANTE, -PAIS_NACIONALIDAD, -PAIS_ORIGEN,
+#          -INTUBADO, -NEUMONIA, -UCI, -OTRO_CASO, -RESULTADO,
+#          -MUNICIPIO_RES, -NACIONALIDAD) %>%
+#   pivot_longer(cols = c(-ENTIDAD_UM, -DEF, -HOSP, -EDAD, -SEXO, -EMBARAZO, -SECTOR, -ID_REGISTRO),
+#                names_to = "factor_riesgo", values_to = "valor") %>%
+#   mutate(SEXO = replace(SEXO, SEXO %in% c("97", "98", "99"), NA),
+#          EMBARAZO = replace(EMBARAZO, EMBARAZO %in% c("98", "99"), NA),
+#          SECTOR = replace(SECTOR, SECTOR %in% c("99"), NA),
+#          valor = replace(valor, valor %in% c("97", "98", "99"), NA),) %>%
+#   mutate(SEXO = 1*(SEXO == "2"),
+#          EMBARAZO = 1*(EMBARAZO == "1"),
+#          valor = 1*(valor == "1")) %>%
+#   pivot_wider(id_cols = c(ENTIDAD_UM, DEF, HOSP, EDAD, SEXO, EMBARAZO, SECTOR, ID_REGISTRO),
+#               names_from = factor_riesgo, values_from = valor) %>%
+#   select(-ID_REGISTRO) %>%
+#   drop_na
+# d
+# edad_mu <- mean(d$EDAD)
+# edad_sd <- sd(d$EDAD)
+# edad_mu
+# edad_sd
+# 
+# d <- d %>%
+#   mutate(EDAD = scale(EDAD) %>% as.numeric())
+# d
+# 
+# # Sólo efectos fijos
+# m1 <- glm(DEF ~ EDAD + SEXO + EMBARAZO + HABLA_LENGUA_INDIG +
+#             DIABETES + EPOC + ASMA + INMUSUPR +
+#             HIPERTENSION + OTRA_COM + CARDIOVASCULAR +
+#             OBESIDAD + RENAL_CRONICA + TABAQUISMO +
+#             ENTIDAD_UM + SECTOR,
+#           data = d, family = binomial(link = "logit"))
+# summary(m1)
+# m2 <- glm(HOSP ~ EDAD + SEXO + EMBARAZO + HABLA_LENGUA_INDIG +
+#             DIABETES + EPOC + ASMA + INMUSUPR +
+#             HIPERTENSION + OTRA_COM + CARDIOVASCULAR +
+#             OBESIDAD + RENAL_CRONICA + TABAQUISMO +
+#             ENTIDAD_UM + SECTOR,
+#           data = d, family = binomial(link = "logit"))
+# summary(m2)
+# 
+# # Efectos aleatorios para entidad y sectrp
+# m3 <- lme4::glmer(DEF ~ EDAD + SEXO + EMBARAZO + HABLA_LENGUA_INDIG +
+#                     DIABETES + EPOC + ASMA + INMUSUPR +
+#                     HIPERTENSION + OTRA_COM + CARDIOVASCULAR +
+#                     OBESIDAD + RENAL_CRONICA + TABAQUISMO +
+#                     (1|ENTIDAD_UM) + (1|SECTOR),
+#                   data = d, family = binomial(link = "logit"),
+#                   verbose = TRUE)
+# summary(m3)
+# 
+# m4 <- lme4::glmer(HOSP ~ EDAD + SEXO + EMBARAZO + HABLA_LENGUA_INDIG +
+#                     DIABETES + EPOC + ASMA + INMUSUPR +
+#                     HIPERTENSION + OTRA_COM + CARDIOVASCULAR +
+#                     OBESIDAD + RENAL_CRONICA + TABAQUISMO +
+#                     (1|ENTIDAD_UM) + (1|SECTOR),
+#                   data = d, family = binomial(link = "logit"),
+#                   verbose = TRUE)
+# summary(m4)
+# 
+# 
+# predict.fun <- function(m4) {
+#   predict(m4, newdata = d[1,], re.form = NA)   # This is predict.merMod
+#   # fixef(m4)
+# }
+# m.boots <- bootMer(m1, predict.fun, nsim = 10, verbose = TRUE, .progress = "txt")
+
+
+# m3 <- brms::brm(DEF ~ EDAD + SEXO + EMBARAZO + HABLA_LENGUA_INDIG +
+#                   DIABETES + EPOC + ASMA + INMUSUPR +
+#                   HIPERTENSION + OTRA_COM + CARDIOVASCULAR +
+#                   OBESIDAD + RENAL_CRONICA + TABAQUISMO +
+#                   (1|ENTIDAD_UM),
+#                 data = d, family = brms::bernoulli(link = "logit"),
+#                 inits = "0")
+# summary(m3)
+  
+####################################
+
+# Elegir comorbilidades a estudiar
 comorb <- c("HABLA_LENGUA_INDIG", "DIABETES", "EPOC", "ASMA", "INMUSUPR",
             "HIPERTENSION", "OTRA_COM", "CARDIOVASCULAR", "OBESIDAD", "RENAL_CRONICA",
             "TABAQUISMO", "EMBARAZO", "SEXO")
 comorb <- set_names(comorb, comorb)
 
-# Dat <- Dat[,c(comorb, "TIPO_PACIENTE", "UCI", "FECHA_DEF")] %>%
-#   mutate(SEXO = replace(SEXO, SEXO == "1", "f")) %>%
-#   mutate(SEXO = replace(SEXO, SEXO == "2", "h"))
+# Seleccionar columnas y recodificar columna SEXO para que
+# hombre = 1 (conveniente para graficar)
 Dat <- Dat[,c(comorb, "TIPO_PACIENTE", "UCI", "FECHA_DEF", "EDAD")] %>%
   mutate(SEXO = replace(SEXO, SEXO == "1", "f")) %>%
   mutate(SEXO = replace(SEXO, SEXO == "2", "h")) %>%
   mutate(SEXO = replace(SEXO, SEXO == "h", "1")) %>%
   mutate(SEXO = replace(SEXO, SEXO == "f", "2"))
+
+Dat
 
 Res <- Dat %>%
   pivot_longer(cols = c(-TIPO_PACIENTE, -UCI, -FECHA_DEF, -EDAD),
@@ -69,8 +164,8 @@ Res <- Dat %>%
                   mutate(resultado = "Muerte"))
     
   }, .id = "factor_riesgo")
-Res %>%
-  print(n = 200)
+
+# Graficar
 p1 <- Res %>%
   filter(resultado != "Cuidados Intensivos") %>%
   
