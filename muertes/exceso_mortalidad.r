06824ac4744a294d1883698405443b1b2de26763
surh <sur00mx@gmail.com>
2020-09-05 14:30:12 -0700

    Licencia

--- muertes/exceso_mortalidad.r
+++ muertes/exceso_mortalidad.r
@@ -1,3 +1,17 @@
+# (C) Copyright 2020 Sur Herrera Paredes
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+
 library(tidyverse)
 rolling_mean <- tibbletime::rollify(mean, window = 7, na_value = NA)
 
