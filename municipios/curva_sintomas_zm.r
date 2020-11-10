04c7209125915199208013380816cd64afc29702
sur <sur00mx@gmail.com>
2020-11-10 13:03:40 -0800

    Mejorando curva Valle de México

--- municipios/curva_sintomas_zm.r
+++ municipios/curva_sintomas_zm.r
@@ -254,56 +254,74 @@ fecha_final <- max(Dat$fecha)
 fecha_inicio <- parse_date("2020-03-01", format = "%Y-%m-%d")
 
 # Valle de méxico
-p1 <- Dat %>%
-  filter(NOM_ZM %in% zonas_grupos[[1]]) %>%
-  mutate(casos = rolling_mean(sintomas_nuevos),
-         muertes = rolling_mean(muertes_nuevas)) %>%
-  filter(fecha >= fecha_inicio) %>%
-  
-  ggplot(aes(x = fecha)) +
-  geom_rect(aes(xmin = fecha_final - 15, xmax = fecha_final,
-                ymin = -Inf, ymax = Inf),
-            fill = "pink") +
-  annotate("text",
-           x = fecha_final - 7,
-           y = 0.85 * max(Dat$sintomas_nuevos[Dat$NOM_ZM == "Valle de México"]),
-           label = 'italic("Estos\nnúmeros\npueden\naumentar")',
-           hjust = "middle",
-           parse = TRUE) +
-  annotate("text",
-           x = fecha_inicio + 10,
-           y = 0.90 * max(Dat$sintomas_nuevos[Dat$NOM_ZM == "Valle de México"]),
-           label = paste(sum(Dat$sintomas_nuevos[Dat$NOM_ZM == "Valle de México"]), "casos"),
-           hjust = "left",
-           parse = FALSE,
-           size = 6) +
-  geom_bar(aes( y = sintomas_nuevos), width = 1, stat = "identity", color = "#4dac26", fill = "#4dac26") +
-  geom_line(aes(y = casos), size = 2, col = "#b8e186") +
-  ylab(label = "Número de nuevos casos") +
-  xlab(label = "Fecha de inicio de síntomas") +
-  ggtitle(label = "Valle de México") +
-  AMOR::theme_blackbox() +
-  theme(axis.title = element_text(size = 20),
-        axis.text = element_text(size = 10),
-        plot.title = element_text(face = "bold", hjust = 0.5),
-        # axis.text.x = element_text(angle = 90),
-        plot.margin = margin(l = 20, r = 20, t = 10),
-        panel.background = element_blank(),
-        panel.border = element_rect(fill=NA, colour = "black", size = 3),
-        # panel.border = element_blank(),
-        legend.position = "top",
-        legend.text = element_text(size = 12),
-        legend.background = element_blank())
+# p1 <- Dat %>%
+#   filter(NOM_ZM %in% zonas_grupos[[1]]) %>%
+#   mutate(casos = rolling_mean(sintomas_nuevos),
+#          muertes = rolling_mean(muertes_nuevas)) %>%
+#   filter(fecha >= fecha_inicio) %>%
+#   
+#   ggplot(aes(x = fecha)) +
+#   geom_rect(aes(xmin = fecha_final - 15, xmax = fecha_final,
+#                 ymin = -Inf, ymax = Inf),
+#             fill = "pink") +
+#   annotate("text",
+#            x = fecha_final - 7,
+#            y = 0.85 * max(Dat$sintomas_nuevos[Dat$NOM_ZM == "Valle de México"]),
+#            label = 'italic("Estos\nnúmeros\npueden\naumentar")',
+#            hjust = "middle",
+#            parse = TRUE) +
+#   annotate("text",
+#            x = fecha_inicio + 10,
+#            y = 0.90 * max(Dat$sintomas_nuevos[Dat$NOM_ZM == "Valle de México"]),
+#            label = paste(sum(Dat$sintomas_nuevos[Dat$NOM_ZM == "Valle de México"]), "casos"),
+#            hjust = "left",
+#            parse = FALSE,
+#            size = 6) +
+#   geom_bar(aes( y = sintomas_nuevos), width = 1, stat = "identity", color = "#4dac26", fill = "#4dac26") +
+#   geom_line(aes(y = casos), size = 2, col = "#b8e186") +
+#   ylab(label = "Número de nuevos casos") +
+#   xlab(label = "Fecha de inicio de síntomas") +
+#   ggtitle(label = "Valle de México") +
+#   AMOR::theme_blackbox() +
+#   theme(axis.title = element_text(size = 20),
+#         axis.text = element_text(size = 10),
+#         plot.title = element_text(face = "bold", hjust = 0.5),
+#         # axis.text.x = element_text(angle = 90),
+#         plot.margin = margin(l = 20, r = 20, t = 10),
+#         panel.background = element_blank(),
+#         panel.border = element_rect(fill=NA, colour = "black", size = 3),
+#         # panel.border = element_blank(),
+#         legend.position = "top",
+#         legend.text = element_text(size = 12),
+#         legend.background = element_blank())
 # p1
 # ggsave("test.png", p1, width = 7, height = 6.7, dpi = 75)
+# archivo <- file.path(args$dir_salida, "vm_casos.png")
+# ggsave(archivo, p1, width = 7, height = 6.7, dpi = 75)
+# archivo <- file.path(args$dir_salida, "vm_casos@2x.png")
+# ggsave(archivo, p1, width = 7, height = 6.7, dpi = 150)
+
+p1 <- graficar_zms(Dat = Dat, zonas_elegidas = zonas_grupos[[1]],
+             fecha_inicio = fecha_inicio,
+             fecha_final = fecha_final) +
+  scale_x_date(date_breaks = "1 month", date_labels = "%b") +
+  ggtitle(label = "Valle de México") +
+  AMOR::theme_blackbox() +
+  theme(panel.background = element_blank(),
+        panel.border = element_rect(fill = NA, color = "black", size = 3),
+        legend.position = "bottom",
+        legend.text = element_text(size = 12),
+        legend.background = element_blank(),
+        axis.title = element_text(size = 20),
+        axis.title.x = element_blank(),
+        axis.text = element_text(size = 10, color = "black"),
+        axis.text.x = element_text(angle = 90),
+        plot.margin = margin(l = 20, r = 20),
+        plot.title = element_text(face = "bold", hjust = 0.5)) 
 archivo <- file.path(args$dir_salida, "vm_casos.png")
 ggsave(archivo, p1, width = 7, height = 6.7, dpi = 75)
 archivo <- file.path(args$dir_salida, "vm_casos@2x.png")
 ggsave(archivo, p1, width = 7, height = 6.7, dpi = 150)
-
-# graficar_zms(Dat = Dat, zonas_elegidas = zonas_grupos[[1]],
-#              fecha_inicio = fecha_inicio,
-#              fecha_final = fecha_final)
 
 p1 <- graficar_zms(Dat = Dat, zonas_elegidas = zonas_grupos[[2]],
              fecha_inicio = fecha_inicio,
