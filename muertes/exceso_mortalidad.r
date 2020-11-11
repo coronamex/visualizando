f577727f71ae3439b86a86aab276dea444c3aff5
sur <sur00mx@gmail.com>
2020-11-10 21:46:24 -0800

    Exceso de mortalidad por entidades y nacional con método consolidado

--- muertes/exceso_mortalidad.r
+++ muertes/exceso_mortalidad.r
@@ -57,7 +57,7 @@ calcular_exceso_mortalidad <- function(Dat){
               y_max = max(muertes),
               muertes = sum(muertes),
               .groups = "drop") %>%
-    mutate(exceso.num = muertes - base) %>%
+    mutate(exceso.num = muertes - floor(base)) %>%
     mutate(exceso.perc = 100 * ((muertes / base) - 1)) %>%
     mutate(exceso.num = ifelse(exceso.num < 0, 0, exceso.num),
            exceso.perc = ifelse(exceso.perc < 0, 0, exceso.perc),
@@ -66,55 +66,59 @@ calcular_exceso_mortalidad <- function(Dat){
   return(list(Dat = Dat, extra = extra))
 }
 
-graficar_exceso_mortalidad <- function(d, m){
-  
-  l <- calcular_exceso_mortalidad(d = d, m = m) 
-  y_max <- max(l$d$muertes)
-  
-  l$d %>%
-    ggplot(aes(x = fecha, y = muertes, group = A)) +
-    geom_ribbon(data = l$m %>%
-                  mutate(base = ifelse(base > muertes, muertes, base)),
-                aes(ymin = base, ymax = muertes),
-                fill = "#fdae6b",
-                alpha = 1, size = 0) +
-    geom_line(aes(col = A, size = A)) +
-    scale_color_manual(values = rev(grey.colors(n = 8, start = 0)),
-                       name = "Año") +
-    scale_size_manual(values = c(rep(1,7), 2),
-                      name = "Año") +
-    
-    geom_text(data = l$extra,
-              aes(label = paste0("+",
-                                 scales::comma(exceso.num),
-                                 "\n(+",
-                                 round(exceso.perc),
-                                 "%)\nexceso\nde\nfallecimientos"),
-                  y = 0.8 * y_max), x = parse_date("2020-03-01"),
-              col = "#fdae6b", fontface = "bold", size = 6, alpha = 1) +
-    scale_y_continuous(labels = scales::comma,
-                       breaks = function(limits){
-                         seq(from = round(limits[1], -4),
-                             to = limits[2], by = 1e3)
-                       }, limits = c(0, y_max)) +
-    scale_x_date(date_breaks = "1 month",
-                 date_labels = "%b") +
-    ylab("Defunciones por todas las causas") +
-    xlab("Mes de defunción") +
-    AMOR::theme_blackbox() +
-    theme(axis.title = element_text(size = 20),
-          axis.text = element_text(size = 10),
-          axis.text.x = element_text(vjust = 0.5),
-          plot.margin = margin(l = 20, r = 20),
-          panel.background = element_blank(),
-          panel.border = element_rect(fill=NA, colour = "black", size = 6),
-          legend.position = "top",
-          legend.text = element_text(size = 12),
-          legend.background = element_blank(),
-          legend.key = element_blank())
-}
+# graficar_exceso_mortalidad <- function(d, m){
+#   
+#   l <- calcular_exceso_mortalidad(d = d, m = m) 
+#   y_max <- max(l$d$muertes)
+#   
+#   l$d %>%
+#     ggplot(aes(x = fecha, y = muertes, group = A)) +
+#     geom_ribbon(data = l$m %>%
+#                   mutate(base = ifelse(base > muertes, muertes, base)),
+#                 aes(ymin = base, ymax = muertes),
+#                 fill = "#fdae6b",
+#                 alpha = 1, size = 0) +
+#     geom_line(aes(col = A, size = A)) +
+#     scale_color_manual(values = rev(grey.colors(n = 8, start = 0)),
+#                        name = "Año") +
+#     scale_size_manual(values = c(rep(1,7), 2),
+#                       name = "Año") +
+#     
+#     geom_text(data = l$extra,
+#               aes(label = paste0("+",
+#                                  scales::comma(exceso.num),
+#                                  "\n(+",
+#                                  round(exceso.perc),
+#                                  "%)\nexceso\nde\nfallecimientos"),
+#                   y = 0.8 * y_max), x = parse_date("2020-03-01"),
+#               col = "#fdae6b", fontface = "bold", size = 6, alpha = 1) +
+#     scale_y_continuous(labels = scales::comma,
+#                        breaks = function(limits){
+#                          seq(from = round(limits[1], -4),
+#                              to = limits[2], by = 1e3)
+#                        }, limits = c(0, y_max)) +
+#     scale_x_date(date_breaks = "1 month",
+#                  date_labels = "%b") +
+#     ylab("Defunciones por todas las causas") +
+#     xlab("Mes de defunción") +
+#     AMOR::theme_blackbox() +
+#     theme(axis.title = element_text(size = 20),
+#           axis.text = element_text(size = 10),
+#           axis.text.x = element_text(vjust = 0.5),
+#           plot.margin = margin(l = 20, r = 20),
+#           panel.background = element_blank(),
+#           panel.border = element_rect(fill=NA, colour = "black", size = 6),
+#           legend.position = "top",
+#           legend.text = element_text(size = 12),
+#           legend.background = element_blank(),
+#           legend.key = element_blank())
+# }
 
-exceso_mortalidad_region <- function(Dat, regiones, nrow = 3){
+graficar_exceso_mortalidad <- function(Dat, regiones,
+                                       exceso.size = 6,
+                                       exceso.fecha = "2020-03-01",
+                                       lineas.sizes = c(1, 2),
+                                       nrow = 3){
 
   # Seleccionar datos y calcular exceso de mortalidad
   Res <- Dat %>%
@@ -132,27 +136,24 @@ exceso_mortalidad_region <- function(Dat, regiones, nrow = 3){
     geom_line(aes(col = A, size = A)) +
     scale_color_manual(values = rev(grey.colors(n = 8, start = 0)),
                        name = "Año") +
-    scale_size_manual(values = c(rep(1,7), 2),
+    scale_size_manual(values = c(rep(lineas.sizes[1],7), lineas.sizes[2]),
                       name = "Año") +
     geom_text(data = Res$extra,
               aes(label = paste0("+",
-                                 scales::comma(exceso.num),
+                                 scales::comma(exceso.num, accuracy = 1),
                                  "\n(+",
                                  round(exceso.perc),
                                  "%)"),
-                  y = 0.8 * y_max), x = parse_date("2020-10-01"),
-              col = "#fdae6b", fontface = "bold", size = 3, alpha = 1) +
+                  y = 0.8 * y_max), x = parse_date(exceso.fecha),
+              col = "#fdae6b", fontface = "bold",
+              size = exceso.size,
+              alpha = 1) +
     scale_y_continuous(labels = scales::comma,
-                       # breaks = function(limits){
-                       #   seq(from = round(limits[1], -4),
-                       #       to = limits[2], by = 1e4)
-                       # },
                        limits = function(limits){
                          limits[1] <- 0
                          limits
                        }) +
-    # scale_x_date(breaks = sort(unique(d$fecha_dummy))[-3],
-    #              labels = scales::date_format("%b")) +
+    scale_x_date(date_breaks = "1 month", date_labels = "%b") +
     ylab("Defunciones por todas las causas") +
     xlab("Mes de defunción") +
     AMOR::theme_blackbox() +
@@ -197,27 +198,15 @@ Dat <- Defs %>%
   filter(!(CVE_ENT %in% c("33", "34", "35"))) %>%
   select(fecha, CVE_ENT, muertes = def_registradas) %>%
   bind_rows(Pand %>% rename(muertes = def_registradas))
-
-Dat <- Dat %>%
-  rename(region = CVE_ENT)
+Dat
 
 
-# Defunciones nacionales previas
-d <- Defs %>%
-  filter(!(CVE_ENT %in% c("33", "34", "35"))) %>%
-  group_by(fecha) %>%
-  # summarise(muertes = sum(def_residentes), .groups = "drop")
-  summarise(muertes = sum(def_registradas), .groups = "drop")
-
-# Defunciones nacionales este año
-m <- Pand %>%
-  group_by(fecha) %>%
-  summarise(muertes = sum(def_registradas),
-            .groups = 'drop')
-
-p1 <- graficar_exceso_mortalidad(d = d, m = m)
-# p1
-# ggsave("test.png", p1, width = 7, height = 6.7, dpi = 75)
+p1 <- graficar_exceso_mortalidad(Dat = Dat %>%
+                           group_by(fecha) %>%
+                           summarise(muertes = sum(muertes),
+                                     .groups = 'drop') %>%
+                           mutate(region = "País"),
+                         regiones = "País")
 archivo <- file.path(args$dir_salida, "exceso_mortalidad_nacional.png")
 ggsave(archivo, p1, width = 7, height = 6.7, dpi = 75)
 archivo <- file.path(args$dir_salida, "exceso_mortalidad_nacional@2x.png")
@@ -231,62 +220,50 @@ stop_for_problems(estados_lut)
 estados_lut <- set_names(estados_lut$X2, estados_lut$X1)
 estados_lut <- estados_lut[1:32]
 
-d <- Defs %>%
-  filter(!(CVE_ENT %in% c("33", "34", "35"))) %>%
-  group_by(CVE_ENT, fecha) %>%
-  # summarise(muertes = sum(def_residentes),
-  #           .groups = "drop") %>%
-  summarise(muertes = sum(def_registradas),
-            .groups = "drop") %>%
-  mutate(estado = as.character(estados_lut[CVE_ENT])) %>%
-  select(-CVE_ENT)
-d
 
-# m <- Li %>%
-#   filter(CVE_ENT != "39") %>%
-#   group_by(CVE_ENT, anio, mes) %>%
-#   summarise(muertes = sum(muertes),
-#             .groups = "drop") %>%
-#   mutate(estado = as.character(estados_lut[CVE_ENT])) %>%
-#   select(-CVE_ENT)
-m <- Pand %>%
-  mutate(estado = as.character(estados_lut[CVE_ENT])) %>%
+Dat <- Dat %>%
+  mutate(region = as.character(estados_lut[CVE_ENT])) %>%
   select(-CVE_ENT)
 
-
-p1 <- exceso_mortalidad_region(d = d, m = m,
-                               estados = as.character(sort(estados_lut)[1:8]))
-# p1
-# ggsave("test.png", p1, width = 7, height = 6.7, dpi = 75)
+p1 <- graficar_exceso_mortalidad(Dat = Dat,
+                                 regiones = as.character(sort(estados_lut)[1:8]),
+                                 exceso.size = 3,
+                                 exceso.fecha = "2020-03-01",
+                                 lineas.sizes = c(0.3, 0.3),
+                                 nrow = 3)
 archivo <- file.path(args$dir_salida, "exceso_mortalidad_estados1.png")
 ggsave(archivo, p1, width = 7, height = 6.7, dpi = 75)
 archivo <- file.path(args$dir_salida, "exceso_mortalidad_estados1@2x.png")
 ggsave(archivo, p1, width = 7, height = 6.7, dpi = 150)
 
-p1 <- exceso_mortalidad_region(d = d, m = m,
-                               estados = as.character(sort(estados_lut)[9:16]))
-# p1
-# ggsave("test.png", p1, width = 7, height = 6.7, dpi = 75)
-archivo <- file.path(args$dir_salida, "exceso_mortalidad_estados2.png")
+p1 <- graficar_exceso_mortalidad(Dat = Dat,
+                                 regiones = as.character(sort(estados_lut)[9:16]),
+                                 exceso.size = 3,
+                                 exceso.fecha = "2020-03-01",
+                                 lineas.sizes = c(0.3, 0.3),
+                                 nrow = 3)archivo <- file.path(args$dir_salida, "exceso_mortalidad_estados2.png")
 ggsave(archivo, p1, width = 7, height = 6.7, dpi = 75)
 archivo <- file.path(args$dir_salida, "exceso_mortalidad_estados2@2x.png")
 ggsave(archivo, p1, width = 7, height = 6.7, dpi = 150)
 
 
-p1 <- exceso_mortalidad_region(d = d, m = m,
-                               estados = as.character(sort(estados_lut)[17:24]))
-# p1
-# ggsave("test.png", p1, width = 7, height = 6.7, dpi = 75)
-archivo <- file.path(args$dir_salida, "exceso_mortalidad_estados3.png")
+p1 <- graficar_exceso_mortalidad(Dat = Dat,
+                                 regiones = as.character(sort(estados_lut)[17:24]),
+                                 exceso.size = 3,
+                                 exceso.fecha = "2020-03-01",
+                                 lineas.sizes = c(0.3, 0.3),
+                                 nrow = 3)archivo <- file.path(args$dir_salida, "exceso_mortalidad_estados3.png")
 ggsave(archivo, p1, width = 7, height = 6.7, dpi = 75)
 archivo <- file.path(args$dir_salida, "exceso_mortalidad_estados3@2x.png")
 ggsave(archivo, p1, width = 7, height = 6.7, dpi = 150)
 
 
-p1 <- exceso_mortalidad_region(d = d, m = m,
-                               estados = as.character(sort(estados_lut)[25:32]))
-# p1
-# ggsave("test.png", p1, width = 7, height = 6.7, dpi = 75)
+p1 <- graficar_exceso_mortalidad(Dat = Dat,
+                                 regiones = as.character(sort(estados_lut)[25:32]),
+                                 exceso.size = 3,
+                                 exceso.fecha = "2020-03-01",
+                                 lineas.sizes = c(0.3, 0.3),
+                                 nrow = 3)
 archivo <- file.path(args$dir_salida, "exceso_mortalidad_estados4.png")
 ggsave(archivo, p1, width = 7, height = 6.7, dpi = 75)
 archivo <- file.path(args$dir_salida, "exceso_mortalidad_estados4@2x.png")
