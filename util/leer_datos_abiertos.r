040521f3783dbad63c59796f25ca2f374e5eda12
sur <sur00mx@gmail.com>
2020-11-27 20:33:00 -0800

    incluyendo datos de antígenos

--- util/leer_datos_abiertos.r
+++ util/leer_datos_abiertos.r
@@ -5,10 +5,12 @@ library(tidyverse)
 #' @param archivo 
 #' @param solo_confirmados 
 #' @param solo_fallecidos 
-#' @param solo_laboratorio sí se deben considerar como confirmados sólo los casos
-#' con prueba positiva PCR, o se deben considerar también las dicatminaciones y
-#' las asociaciones. Sólo aplica para versión octubre2020.
-#' @param version "adivinar", "abril2020", o "octubre2020".
+#' @param solo_laboratorio Para la versión octubre2020 indica sí se deben
+#' considerar como confirmados sólo los casos con prueba positiva PCR,
+#' o se deben considerar también las dicatminaciones y las asociaciones.
+#' Para la versión noviembre2020 incluye las pruebas de antígeno.
+#' No aplica para la version abril2020.
+#' @param version "adivinar", "abril2020", "octubre2020" o "noviembre2020".
 #'
 #' @return
 #' @export
@@ -34,13 +36,17 @@ leer_datos_abiertos <- function(archivo,
            PAIS_ORIGEN = parse_character(PAIS_ORIGEN, na = c("97", "", "NA"))) 
   
   if(version == "adivinar"){
-    variables_variables <- c("RESULTADO", "RESULTADO_LAB", "CLASIFICACION_FINAL", "INDIGENA")
+    variables_variables <- c("RESULTADO", "RESULTADO_LAB",
+                             "CLASIFICACION_FINAL", "INDIGENA",
+                             "TOMA_MUESTRA_ANTIGENO", "RESULTADO_ANTIGENO")
     variables_variables <- variables_variables %in% colnames(Dat)
     
-    if( all(variables_variables[c(2,3,4)]) & !any(variables_variables[c(1)]) ){
+    if( all(variables_variables[c(2,3,4)]) & !any(variables_variables[c(1,5,6)]) ){
       version <- "octubre2020"
-    }else if( all(variables_variables[c(1)]) & !any(variables_variables[c(2,3,4)]) ){
+    }else if( all(variables_variables[c(1)]) & !any(variables_variables[c(2,3,4,5,6)]) ){
       version <- "abril2020"
+    }else if( all(variables_variables[c(2,3,4,5,6)]) & !any(variables_variables[c(1)]) ){
+      version <- "noviembre2020"
     }else{
       stop("ERROR: no puedo adivinar versión de la base de datos", call. = TRUE)
     }
@@ -50,16 +56,26 @@ leer_datos_abiertos <- function(archivo,
   if(solo_confirmados){
     if(version == "octubre2020"){
       if(solo_laboratorio){
+        # Sólo PCR
         res_vals <- c("3")
       }else{
         res_vals <- c("1", "2", "3")
       }
       Dat <- Dat %>%
         filter(CLASIFICACION_FINAL %in% res_vals)
-   
     }else if(version == "abril2020"){
       Dat <- Dat %>%
         filter(RESULTADO == "1")
+    }else if(version == "noviembre2020"){
+      # Fecha en que se incluyen pruebas de antígeno
+      if(solo_laboratorio){
+        # PCR y antígeno
+        res_vals <- c("3", "8")
+      }else{
+        res_vals <- c("1", "2", "3", "8")
+      }
+      Dat <- Dat %>%
+        filter(CLASIFICACION_FINAL %in% res_vals)
     }else{
       stop("ERROR: versión inválida", call. = TRUE)
     }
