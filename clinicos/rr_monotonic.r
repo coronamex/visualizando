8c7aa3e3d1b4f76f405b76a917dccbbbdb2f4ae9
sur <sur00mx@gmail.com>
2021-07-26 17:10:20 -0700

    Limpiando

--- clinicos/rr_monotonic.r
+++ clinicos/rr_monotonic.r
@@ -26,8 +26,7 @@ Dat <- leer_datos_abiertos(archivo = args$base_de_datos,
                            solo_fallecidos = FALSE,
                            solo_laboratorio = FALSE,
                            version = "adivinar")
-
-
+cat("RR por edad y mes...\n")
 
 # LUT de nombres de variables
 rr_lut <- set_names(c("Habla lengua indígena", "Diabetes", "EPOC", "Asma", "Inmnosupresión",
@@ -38,79 +37,15 @@ rr_lut <- set_names(c("Habla lengua indígena", "Diabetes", "EPOC", "Asma", "Inm
                       "HIPERTENSION", "OTRA_COM", "CARDIOVASCULAR", "OBESIDAD", "RENAL_CRONICA",
                       "TABAQUISMO", "EMBARAZO", "SEXO", "EDAD", "INDIGENA"))
 
-# Eliminar casos recientes
+# Eliminar casos muy recients recientes, y muy viejos
 Dat <- Dat %>%
   filter(FECHA_SINTOMAS > args$fecha_inicio) %>%
   # filter(FECHA_SINTOMAS >= args$fecha_parteaguas) %>%
   filter(FECHA_SINTOMAS < Sys.Date() - 15 | !is.na(FECHA_DEF))
 # Dat
 
-# # Seleccionar datos y convertir variables a indicadores
-# d <- Dat %>%
-#   mutate(DEF = 1*(!is.na(FECHA_DEF)),
-#          TIPO_PACIENTE = replace(TIPO_PACIENTE, TIPO_PACIENTE == "99", NA),
-#          Mes = format(FECHA_SINTOMAS, "%m-%Y")) %>%
-#   # select(TIPO_PACIENTE) %>% table(useNA = "a")
-#   mutate(HOSP = 1*(TIPO_PACIENTE == "2")) %>%
-#   select(names(rr_lut), DEF, TIPO_PACIENTE, HOSP, ENTIDAD_UM, SECTOR, ID_REGISTRO, Mes) %>%
-#   
-#   # select(-FECHA_ACTUALIZACION, -FECHA_INGRESO, -FECHA_SINTOMAS, -FECHA_DEF,
-#   #        -ORIGEN, -TIPO_PACIENTE,
-#   #        -ENTIDAD_NAC, -ENTIDAD_RES, -MIGRANTE, -PAIS_NACIONALIDAD, -PAIS_ORIGEN,
-#   #        -INTUBADO, -NEUMONIA, -UCI, -OTRO_CASO, -RESULTADO,
-#   #        -MUNICIPIO_RES, -NACIONALIDAD) 
-#   pivot_longer(cols = c(-ENTIDAD_UM, -DEF, -HOSP, -EDAD, -SEXO, -EMBARAZO, -SECTOR, -ID_REGISTRO, -Mes),
-#                names_to = "factor_riesgo", values_to = "valor") %>%
-#   mutate(SEXO = replace(SEXO, SEXO %in% c("97", "98", "99"), NA),
-#          EMBARAZO = replace(EMBARAZO, EMBARAZO %in% c("98", "99"), NA),
-#          SECTOR = replace(SECTOR, SECTOR %in% c("99"), NA),
-#          valor = replace(valor, valor %in% c("97", "98", "99"), NA)) %>%
-#   mutate(SEXO = 1*(SEXO == "2"),
-#          EMBARAZO = 1*(EMBARAZO == "1"),
-#          valor = 1*(valor == "1")) %>%
-#   pivot_wider(id_cols = c(ENTIDAD_UM, DEF, HOSP, EDAD, SEXO, EMBARAZO, SECTOR, ID_REGISTRO, Mes),
-#               names_from = factor_riesgo, values_from = valor) %>%
-#   select(-ID_REGISTRO) %>%
-#   drop_na %>%
-#   mutate(EDAD = round(EDAD, -1))
-# 
-# d
-# 
-# ftable(DEF ~ Mes, d)
-
-# m1 <- brm(DEF ~ mo(EDAD)*Mes + SEXO, data = d, chains = 4, cores = 4, warmup = 500, iter = 1000)
-# m2 <- brm(DEF ~ mo(EDAD)*Mes + SEXO, data = d, chains = 4, cores = 4, warmup = 500, iter = 1000, family = bernoulli(link = "logit"))
-
-# d
-# d <- d %>%
-#   select(DEF, EDAD, Mes, ENTIDAD_UM, SEXO, EMBARAZO,
-#          SECTOR, HABLA_LENGUA_INDIG, DIABETES, EPOC, ASMA,
-#          INMUSUPR, HIPERTENSION, CARDIOVASCULAR, OBESIDAD,
-#          RENAL_CRONICA, TABAQUISMO, INDIGENA, OTRA_COM) %>%
-#   group_by(EDAD, Mes, ENTIDAD_UM, SEXO, EMBARAZO,
-#            SECTOR, HABLA_LENGUA_INDIG, DIABETES, EPOC, ASMA,
-#            INMUSUPR, HIPERTENSION, CARDIOVASCULAR, OBESIDAD,
-#            RENAL_CRONICA, TABAQUISMO, INDIGENA, OTRA_COM) %>%
-#   summarise(defs = sum(DEF),
-#             n_ind = length(DEF),
-#             .groups = 'drop') %>%
-#     mutate(EDAD = factor(EDAD, levels = sort(unique(d$EDAD)), ordered = TRUE)) 
-# d
-# 
-# m3 <- brm(defs | trials(n_ind) ~ mo(EDAD)*Mes + SEXO + EMBARAZO +
-#             HABLA_LENGUA_INDIG + INDIGENA +
-#             DIABETES + EPOC + ASMA + INMUSUPR +
-#             HIPERTENSION + OTRA_COM + CARDIOVASCULAR +
-#             OBESIDAD + RENAL_CRONICA + TABAQUISMO +
-#             ENTIDAD_UM + SECTOR, data = d,
-#           chains = 4, cores = 4, warmup = 500, iter = 1000,
-#           family = binomial(link = "logit"))
-# summary(m3)
-# conditional_effects(m3, "EDAD:Mes")
-
-
-
-# Seleccionar datos y convertir variables a indicadores
+# Seleccionar datos, convertir variables a indicadores,
+# procesar edad, etc
 d4 <- Dat %>%
   mutate(DEF = 1*(!is.na(FECHA_DEF)),
          TIPO_PACIENTE = replace(TIPO_PACIENTE, TIPO_PACIENTE == "99", NA),
@@ -166,43 +101,6 @@ res <- res %>%
             lower = exp(lower__),
             upper = exp(upper__)) %>%
   mutate(Mes = parse_date(as.character(Mes), format = "%m-%Y")) 
-
-# res %>%
-#   ggplot(aes(x = Edad, y = Estimate, col = Mes)) +
-#   geom_point() +
-#   geom_errorbar(aes(ymin = lower, ymax = upper), width = 0.2) +
-#   scale_y_continuous(name = "Mes (2021)") +
-#   AMOR::theme_blackbox()
-# 
-# res %>%
-#   ggplot(aes(x = Mes, y = Estimate, col = Edad)) +
-#   geom_point() +
-#   geom_line() +
-#   geom_errorbar(aes(ymin = lower, ymax = upper), width = 0.2) +
-#   AMOR::theme_blackbox()
-# 
-# 
-# res %>%
-#   ggplot(aes(x = Edad, y = Estimate)) +
-#   facet_wrap(~ Mes, scales = "free_y") +
-#   geom_point() +
-#   geom_vline(xintercept = 6, col = "darkgrey") +
-#   geom_hline(yintercept = 1.2, col = "darkgrey") +
-#   geom_errorbar(aes(ymin = lower, ymax = upper), width = 0.2) +
-#   theme_classic()
-# 
-# 
-# res %>%
-#   ggplot(aes(x = Mes, y = Estimate)) +
-#   facet_wrap(~ Edad, scales = "free_y") +
-#   geom_point() +
-#   # geom_vline(xintercept = 6, col = "darkgrey") +
-#   # geom_hline(yintercept = 1.2, col = "darkgrey") +
-#   geom_errorbar(aes(ymin = lower, ymax = upper), width = 0.2) +
-#   scale_x_date(date_breaks = "1 month", date_labels = "%b") +
-#   theme_classic()
-
-  
 
 p1 <- res %>%
   mutate(grupo = Edad > 40) %>%
