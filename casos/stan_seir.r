b34e3aef9943c5a85aff6aab75b5949212bd3f55
sur <sur00mx@gmail.com>
2021-12-26 09:57:29 -0800

    actualización 2021-12-25

--- casos/stan_seir.r
+++ casos/stan_seir.r
@@ -103,12 +103,12 @@ stan_datos <- list(n_obs = nrow(dat_train),
 #                          0.200, 0.253))
 
 init <- list(logphi = 4.1,
-             r_betas = c(0.33, 0.30,
-                         0.31, 0.23,
-                         0.18, 0.16,
+             r_betas = c(0.35, 0.32,
+                         0.27, 0.20,
+                         0.16, 0.16,
                          0.16, 0.15,
-                         0.16, 0.17,
-                         0.20, 0.19))
+                         0.16, 0.19,
+                         0.21, 0.18))
 
 init <- list(chain_1 = init,
              chain_2 = init,
