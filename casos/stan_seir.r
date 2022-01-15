647379710db9935f61e8a5f417606629739d530e
sur <sur00mx@gmail.com>
2022-01-14 20:48:13 -0800

    Actualización 2022-01-14

--- casos/stan_seir.r
+++ casos/stan_seir.r
@@ -103,12 +103,12 @@ stan_datos <- list(n_obs = nrow(dat_train),
 #                          0.200, 0.253))
 
 init <- list(logphi = 4.1,
-             r_betas = c(0.38, 0.24,
+             r_betas = c(0.38, 0.23,
                          0.19, 0.16,
-                         0.16, 0.16,
+                         0.16, 0.15,
                          0.15, 0.16,
-                         0.19, 0.20,
-                         0.18, 0.42))
+                         0.20, 0.20,
+                         0.18, 0.45))
 
 init <- list(chain_1 = init,
              chain_2 = init,
