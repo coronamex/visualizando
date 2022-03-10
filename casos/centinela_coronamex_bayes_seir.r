d800c59c77f318dc912999d5a6be226115b78c43
Sur Herrera Paredes <surh@sh03-ln03.stanford.edu>
2022-03-09 17:27:16 -0800

    entinela 2022-03-07

--- casos/centinela_coronamex_bayes_seir.r
+++ casos/centinela_coronamex_bayes_seir.r
@@ -150,7 +150,7 @@ fechas_dias <- seq(from=0, to = n_dias_ajuste, by = 15) %>% floor
 # fechas_dias <- fechas_dias[-c(8:9, 11, 19)]
 fechas_dias <- fechas_dias[-c(8:9, 11, 19, 24:25)]
 
-fechas_dias <- fechas_dias[1:(length(fechas_dias) - 1)]
+# fechas_dias <- fechas_dias[1:(length(fechas_dias) - 1)]
 
 fechas_dias
 print(c(fechas_dias, n_dias_ajuste) %>% diff)
@@ -203,9 +203,9 @@ init <- list(logphi = 3.6,
                          0.19, 0.19,
                          0.17, 0.18,
                          0.21, 0.24,
-                         0.22, 0.28,
-                         0.80, 0.30,
-                         0.11, 0.11))
+                         0.22, 0.27,
+                         0.81, 0.31,
+                         0.11, 0.09))
 
 adapt_delta <- 0.5
 max_treedepth <- 10
